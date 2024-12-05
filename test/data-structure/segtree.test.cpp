777ede2aadea1fe9a77bd105b53de5c29f72dab6
Daylight <33833751+daylight-pro@users.noreply.github.com>
2024-12-05 18:17:09 +0900

    segtree追加

---
+++ test/data-structure/segtree.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_sum"
+
+#include "test/template.hpp"
+#include "src/data-structure/segtree.hpp"
+
+ll op(ll a, ll b) { return min(a, b); }
+ll e() { return 1e9 + 100; }
+int main() {
+   cin.tie(0)->sync_with_stdio(0);
+
+   int N, Q;
+   cin >> N >> Q;
+   vector<ll> A(N);
+   for(auto&& a : A) cin >> a;
+   segtree<ll, op, e> seg(A);
+   while(Q--) {
+      int L, R;
+      cin >> L >> R;
+      cout << seg.prod(L, R) << '\n';
+   }
+}

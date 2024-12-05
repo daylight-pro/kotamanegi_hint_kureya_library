eb1d79c64a2960e239fcb351402be173bc8296cb
Daylight <33833751+daylight-pro@users.noreply.github.com>
2024-12-05 15:57:25 +0900

    バグ修正

--- src/math/floor_sum.hpp
+++ src/math/floor_sum.hpp
@@ -1,6 +1,6 @@
 ll floor_sum(const ll& n, const ll& m, ll a, ll b) {
    ll ret = 0;
-   if(a >= m) ret += (n - 1) * n * (b / m) / 2, a %= m;
+   if(a >= m) ret += (n - 1) * n * (a / m) / 2, a %= m;
    if(b >= m) ret += n * (b / m), b %= m;
    ll y = (a * n + b) / m;
    if(y == 0) return ret;

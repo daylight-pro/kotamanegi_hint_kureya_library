393caaae53825f1120792eb06d7744734760d814
Daylight <33833751+daylight-pro@users.noreply.github.com>
2024-12-15 04:29:06 +0900

    二重辺連結成分分解追加

---
+++ src/graph/bi_connected_components.hpp
@@ -0,0 +1,42 @@
+struct BiConnectedComponents : LowLink {
+   public:
+   using LowLink::bridge;
+   using LowLink::g;
+   using LowLink::low;
+   using LowLink::ord;
+
+   vector<int> comp;
+   vector<vector<int>> tree;
+   vector<vector<int>> group;
+
+   void build(const vector<vector<int>>& g) {
+      comp.assign(size(g), -1);
+      int k = 0;
+      for(int i = 0; i < size(comp); i++) {
+         if(comp[i] == -1) { dfs(i, -1, k); }
+      }
+      group.resize(k);
+      for(int i = 0; i < size(g); i++) { group[comp[i]].push_back(i); }
+      tree.resize(k);
+      for(auto& e : bridge) {
+         tree[comp[e.first]].push_back(comp[e.second]);
+         tree[comp[e.second]].push_back(comp[e.first]);
+      }
+   }
+   vector<vector<int>> getTree() { return tree; }
+   vector<vector<int>> getGroup() { return group; }
+
+   explicit BiConnectedComponents(const vector<vector<int>>& g) : LowLink(g) { build(g); }
+
+   private:
+   vector<int> used;
+   vector<pair<int, int>> tmp;
+
+   void dfs(int cur, int pre, int& k) {
+      if(pre != -1 && ord[pre] >= low[cur]) comp[cur] = comp[pre];
+      else comp[cur] = k++;
+      for(auto to : g[cur]) {
+         if(comp[to] == -1) dfs(to, cur, k);
+      }
+   }
+};

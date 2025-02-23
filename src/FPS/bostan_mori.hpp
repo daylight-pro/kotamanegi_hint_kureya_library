6745e858139053892566f57661f00f7492dc1fee
shinchankosen <sophiaringoukosen@gmail.com>
2025-02-23 16:52:28 +0900

    bostanmori shorter

--- src/FPS/bostan_mori.hpp
+++ src/FPS/bostan_mori.hpp
@@ -1,22 +1,16 @@
-// Bostan-Mori
 // find [x^N] P(x)/Q(x), O(K log K log N)
 // deg(Q(x)) = K, deg(P(x)) < K, Q[0] = 1
-template <typename mint> mint BostanMori(const vector<mint> &P, const vector<mint> &Q, long long N) {
-   assert(!P.empty() && !Q.empty());
-   if (N == 0) return P[0] / Q[0];
-   
-   int qdeg = (int)Q.size();
-   vector<mint> P2{P}, minusQ{Q};
-   P2.resize(qdeg - 1);
-   for (int i = 1; i < (int)Q.size(); i += 2) minusQ[i] = -minusQ[i];
-   P2 = conv(P2, minusQ);
-   vector<mint> Q2 = conv(Q, minusQ);
-   vector<mint> S(qdeg - 1), T(qdeg);
-   for (int i = 0; i < (int)S.size(); ++i) {
-      S[i] = (N % 2 == 0 ? P2[i * 2] : P2[i * 2 + 1]);
+template <typename T> T BostanMori(std::vector<T> P, std::vector<T> Q, ll N) {
+   const int d = Q.size();
+   for (; N; N >>= 1) {
+      auto Q_neg = Q;
+      for (size_t i = 1; i < Q.size(); i += 2) Q_neg[i] *= -1;
+      P = conv(P, Q_neg);
+      Q = conv(Q, Q_neg);
+      for (size_t i = N & 1; i < P.size(); i += 2) P[i >> 1] = P[i];
+      for (size_t i = 0; i < Q.size(); i += 2) Q[i >> 1] = Q[i];
+      P.resize(d - 1);
+      Q.resize(d);
    }
-   for (int i = 0; i < (int)T.size(); ++i) {
-      T[i] = Q2[i * 2];
-   }
-   return BostanMori(S, T, N >> 1);
+   return P[0];
 }

05f3ced322b8cf2d0ac35710468c733299d9814f
Daylight <33833751+daylight-pro@users.noreply.github.com>
2025-02-24 01:54:15 +0900

    テスト追加

--- src/data-structure/beats_example.cpp
+++ src/data-structure/beats_example.cpp
@@ -9,15 +9,14 @@ template<typename Num> inline Num second_highest(Num a, Num a2, Num b, Num b2) n
    return a == b ? std::max(a2, b2) : a2 >= b ? a2 : b2 >= a ? b2 : std::min(a, b);
 }
 
-using BNum = long long;
-constexpr BNum BINF = 1LL << 61;
+constexpr ll BINF = 1LL << 61;
 
 struct S {
-   BNum lo, hi, lo2, hi2, sum;  // 区間最小・最大値，区間最小・最大から二番目の値，区間総和
-   unsigned sz, nlo, nhi;       // 区間要素数，区間最小・最大値をとる要素の個数
+   ll lo, hi, lo2, hi2, sum;  // 区間最小・最大値，区間最小・最大から二番目の値，区間総和
+   unsigned sz, nlo, nhi;     // 区間要素数，区間最小・最大値をとる要素の個数
    bool fail;
    S() : lo(BINF), hi(-BINF), lo2(BINF), hi2(-BINF), sum(0), sz(0), nlo(0), nhi(0), fail(0) {}
-   S(BNum x, unsigned sz_ = 1)
+   S(ll x, unsigned sz_ = 1)
        : lo(x), hi(x), lo2(BINF), hi2(-BINF), sum(x * sz_), sz(sz_), nlo(sz_), nhi(sz_), fail(0) {}
 };
 
@@ -25,7 +24,7 @@ S e() { return S(); }
 
 S op(S l, S r) {
    S ret;
-   ret.lo = std::min(l.lo, r.lo), ret.hi = std::max(l.hi, r.hi);
+   ret.lo = min(l.lo, r.lo), ret.hi = max(l.hi, r.hi);
    ret.lo2 = second_lowest(l.lo, l.lo2, r.lo, r.lo2);
    ret.hi2 = second_highest(l.hi, l.hi2, r.hi, r.hi2);
    ret.sum = l.sum + r.sum, ret.sz = l.sz + r.sz;
@@ -35,17 +34,17 @@ S op(S l, S r) {
 }
 
 struct F {
-   BNum lb, ub, bias;
-   F(BNum chmax_ = -BINF, BNum chmin_ = BINF, BNum add = 0) : lb(chmax_), ub(chmin_), bias(add) {}
-   static F chmin(BNum x) noexcept { return F(-BINF, x, BNum(0)); }
-   static F chmax(BNum x) noexcept { return F(x, BINF, BNum(0)); }
-   static F add(BNum x) noexcept { return F(-BINF, BINF, x); };
+   ll lb, ub, bias;
+   F(ll chmax_ = -BINF, ll chmin_ = BINF, ll add = 0) : lb(chmax_), ub(chmin_), bias(add) {}
+   static F chmin(ll x) noexcept { return F(-BINF, x, ll(0)); }
+   static F chmax(ll x) noexcept { return F(x, BINF, ll(0)); }
+   static F add(ll x) noexcept { return F(-BINF, BINF, x); };
 };
 
 F composition(F fnew, F fold) {
    F ret;
-   ret.lb = std::max(std::min(fold.lb + fold.bias, fnew.ub), fnew.lb) - fold.bias;
-   ret.ub = std::min(std::max(fold.ub + fold.bias, fnew.lb), fnew.ub) - fold.bias;
+   ret.lb = max(min(fold.lb + fold.bias, fnew.ub), fnew.lb) - fold.bias;
+   ret.ub = min(max(fold.ub + fold.bias, fnew.lb), fnew.ub) - fold.bias;
    ret.bias = fold.bias + fnew.bias;
    return ret;
 }
@@ -55,14 +54,14 @@ F id() { return F(); }
 S mapping(F f, S x) {
    if(x.sz == 0) return e();
    else if(x.lo == x.hi or f.lb == f.ub or f.lb >= x.hi or f.ub < x.lo) {
-      return S(std::min(std::max(x.lo, f.lb), f.ub) + f.bias, x.sz);
+      return S(min(max(x.lo, f.lb), f.ub) + f.bias, x.sz);
    } else if(x.lo2 == x.hi) {
-      x.lo = x.hi2 = std::max(x.lo, f.lb) + f.bias;
-      x.hi = x.lo2 = std::min(x.hi, f.ub) + f.bias;
+      x.lo = x.hi2 = max(x.lo, f.lb) + f.bias;
+      x.hi = x.lo2 = min(x.hi, f.ub) + f.bias;
       x.sum = x.lo * x.nlo + x.hi * x.nhi;
       return x;
    } else if(f.lb < x.lo2 and f.ub > x.hi2) {
-      BNum nxt_lo = std::max(x.lo, f.lb), nxt_hi = std::min(x.hi, f.ub);
+      ll nxt_lo = max(x.lo, f.lb), nxt_hi = min(x.hi, f.ub);
       x.sum += (nxt_lo - x.lo) * x.nlo - (x.hi - nxt_hi) * x.nhi + f.bias * x.sz;
       x.lo = nxt_lo + f.bias, x.hi = nxt_hi + f.bias, x.lo2 += f.bias, x.hi2 += f.bias;
       return x;
@@ -70,5 +69,5 @@ S mapping(F f, S x) {
    x.fail = 1;
    return x;
 }
-using segtree = atcoder::lazy_segtree<S, op, e, F, mapping, composition, id>;
+using segtree = lazy_segtree<S, op, e, F, mapping, composition, id>;
 }  // namespace RangeChMinMaxAddSum
